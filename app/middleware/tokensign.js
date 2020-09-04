2f678a83cc2c7001d98eb4a4c4b7fc1128959057
nicholaslowie21 <nicholaslowie@comp.nus.edu.sg>
2020-09-04 23:47:36 +0700

    minor change

--- app/middleware/tokensign.js
+++ app/middleware/tokensign.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
 const env = require('../config/env');
 
-module.exports = function(id, username, role, type) {
-    console.log('Token sign id, username, roles: ', id, username, role, type);
+module.exports = function(id, role, type) {
+    console.log('Token sign id, role, type: ', id, role, type);
     
-    return jwt.sign({ id: id, username: username, role: role, type: type }, env.jwtSecret);
+    return jwt.sign({ id: id, role: role, type: type }, env.jwtSecret);
 }
