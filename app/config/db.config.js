a8722bb619025d4298d69b7f8fec5991cdd2ad04
nicholaslowie21 <nicholaslowie@comp.nus.edu.sg>
2020-09-14 00:19:23 +0700

    added atlas mongodb

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,6 @@
 module.exports = {
     url: "mongodb://localhost:27017/dev-UN-db"
+    // url: "mongodb+srv://dev-un:qXcmImsoE0ARKze9@cluster0.zwevy.mongodb.net/dev-un?retryWrites=true&w=majority"
 };
+
+// mongodb+srv://dev-un:qXcmImsoE0ARKze9@cluster0.zwevy.mongodb.net/dev-un?retryWrites=true&w=majority
