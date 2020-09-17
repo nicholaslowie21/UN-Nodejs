b5c174fb810e17cf89b04f5e858aa3a66996fce6
nicholaslowie21 <nicholaslowie@comp.nus.edu.sg>
2020-09-17 23:05:56 +0700

    db url typo

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     // url: "mongodb://localhost:27017/dev-UN-db"
-    url: "mongodb+srv://demo-UN:bpjOghMtTXr49WqE@kocosd.ejiji.mongodb.net/demo-UN?retryWrites=true&w=majority"
+    url: "mongodb+srv://dbUser:bpjOghMtTXr49WqE@kocosd.ejiji.mongodb.net/demo-UN?retryWrites=true&w=majority"
 };
 // url: "mongodb+srv://dbUser:bpjOghMtTXr49WqE@kocosd.ejiji.mongodb.net/<dbname>?retryWrites=true&w=majority"
