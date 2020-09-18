4ce77e0a13187bcff9768e6ac03fb306ad75b4fc
nicholaslowie21 <nicholaslowie@comp.nus.edu.sg>
2020-09-18 22:49:56 +0700

    fix

--- app/routes/project.route.js
+++ app/routes/project.route.js
@@ -6,7 +6,7 @@ const auth = require('../middleware/auth')
 const ProjectValidator = require('../validator/project.validator');
 const Helper = require('../service/helper.service');
 
-router.get('/viewProject', auth, ProjectValidator.viewProject, Helper.ifErrors, projectController.viewProject);
+router.get('/viewProject', ProjectValidator.viewProject, Helper.ifErrors, projectController.viewProject);
 
 router.get('/searchProjectsCode', auth, ProjectValidator.searchProjects, Helper.ifErrors, projectController.searchProjects)
 
