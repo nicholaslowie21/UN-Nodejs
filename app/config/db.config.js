ab0f886afd713e7d1ad24d9ded177b0127152b07
nicholaslowie21 <nicholaslowie@comp.nus.edu.sg>
2020-10-25 14:42:30 +0700

    db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     // url: "mongodb://localhost:27017/dev-UN-db"
     // url: "mongodb://localhost:27017/dev-UN-db-clean"
-    url: "mongodb+srv://dbUser:bpjOghMtTXr49WqE@kocosd.ejiji.mongodb.net/3SR-demoDB?retryWrites=true&w=majority"
+    // url: "mongodb+srv://dbUser:bpjOghMtTXr49WqE@kocosd.ejiji.mongodb.net/3SR-demoDB?retryWrites=true&w=majority"
+    url:"mongodb+srv://dbUser:bpjOghMtTXr49WqE@kocosd.ejiji.mongodb.net/3SR-demoDB?retryWrites=true&w=majority"
 };
 // url: "mongodb+srv://dbUser:bpjOghMtTXr49WqE@kocosd.ejiji.mongodb.net/<dbname>?retryWrites=true&w=majority"
