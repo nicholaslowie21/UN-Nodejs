9c9e0f286c0dab88ddcf9218afb49cd61464eb9a
nicholaslowie21 <nicholaslowie@comp.nus.edu.sg>
2020-11-27 14:47:33 +0700

    fix badges bug

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    url: "mongodb://localhost:27017/dev-UN-db"
+    // url: "mongodb://localhost:27017/dev-UN-db"
     // url: "mongodb://localhost:27017/dev-UN-db"
     // url: "mongodb://localhost:27017/dev-UN-db-clean"
-    // url: "mongodb+srv://dbUser:bpjOghMtTXr49WqE@kocosd.ejiji.mongodb.net/FSR-demoDB?retryWrites=true&w=majority"
+    url: "mongodb+srv://dbUser:bpjOghMtTXr49WqE@kocosd.ejiji.mongodb.net/FSR-demoDB?retryWrites=true&w=majority"
 };
 //  url: "mongodb+srv://dbUser:bpjOghMtTXr49WqE@kocosd.ejiji.mongodb.net/demo-UN?retryWrites=true&w=majority"
