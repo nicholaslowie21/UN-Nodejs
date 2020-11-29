cc6af5683e7d55d1abacad3c5cfa941e4c89012c
nicholaslowie21 <nicholaslowie@comp.nus.edu.sg>
2020-11-29 17:53:05 +0700

    minor  message fix

--- app/validator/verification.validator.js
+++ app/validator/verification.validator.js
@@ -4,23 +4,23 @@ const Users = db.users;
 
 exports.retrieveList = [
     body('role').exists().custom(async value => {
-        if (value == 'user') 
-            return Promise.reject('You are not authorised to retrieve this list!')
+        if (value != 'admin' && value != 'adminlead' && value != 'regionaladmin') 
+            return Promise.reject('You are not authorised to perform this action!')
     })
 ]
 
 exports.verifyInstitution = [
     body('role').exists().custom(async value => {
-        if (value == 'user') 
-            return Promise.reject('You are not authorised to retrieve this list!')
+        if (value != 'admin' && value != 'adminlead' && value != 'regionaladmin') 
+            return Promise.reject('You are not authorised to perform this action!')
     }),
     body('institutionId').exists()
 ]
 
 exports.declineUserRequest = [
     body('role').exists().custom(async value => {
-        if (value == 'user') 
-            return Promise.reject('You are not authorised to retrieve this list!')
+        if (value != 'admin' && value != 'adminlead' && value != 'regionaladmin') 
+            return Promise.reject('You are not authorised to perform this action!')
     }),
     body('requestId').exists()
 ]
