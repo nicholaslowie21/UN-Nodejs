6d6f1d1bb3c4a79f74656f8b8e5329ada7cf6043
nicholaslowie21 <nicholaslowie@comp.nus.edu.sg>
2020-12-03 10:49:20 +0700

    added some mapping message and auth

--- app/routes/mapping.route.js
+++ app/routes/mapping.route.js
@@ -6,8 +6,8 @@ const auth = require('../middleware/auth')
 const MappingValidator = require('../validator/mapping.validator');
 const Helper = require('../service/helper.service');
 
-router.post('/csv/users', mappingController.csvUser, mappingController.addUserCSV)
-router.post('/csv/institutions', mappingController.csvInstitution, mappingController.addInstitutionCSV)
+router.post('/csv/users', auth, mappingController.csvUser, mappingController.addUserCSV)
+router.post('/csv/institutions', auth, mappingController.csvInstitution, mappingController.addInstitutionCSV)
 
 router.post('/claim', mappingController.claimUpload, MappingValidator.accountClaim, mappingController.accountClaim )
 
